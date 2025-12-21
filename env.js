0cd02bbbb658d42508d815a79f44496370fc6282
ericbrustad <ericbrustad@gmail.com>
2025-12-21 16:16:04 -0600

    s

--- env.js
+++ env.js
@@ -1,8 +1,9 @@
 // Environment Configuration
 window.ENV = {
-  SUPABASE_URL: 'https://siumiadylwcrkaqsfwkj.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpdW1pYWR5bHdjcmthcXNmd2tqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2NjMzMTMsImV4cCI6MjA4MTIzOTMxM30.sSZBsXyOOmIp2eve_SpiUGeIwx3BMoxvY4c7bvE2kKw',
-  SUPABASE_UID: '99d34cd5-a593-4362-9846-db7167276592',
-  SUPABASE_EMAIL: 'ericbrustad@gmail.com',
-  SUPABASE_PASSWORD: 'Newhouse2025!'
+  SUPABASE_URL: https://siumiadylwcrkaqsfwkj.supabase.co,
+  SUPABASE_ANON_KEY: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpdW1pYWR5bHdjcmthcXNmd2tqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2NjMzMTMsImV4cCI6MjA4MTIzOTMxM30.sSZBsXyOOmIp2eve_SpiUGeIwx3BMoxvY4c7bvE2kKw,
+  SUPABASE_UID: 99d34cd5-a593-4362-9846-db7167276592,
+  SUPABASE_EMAIL: ericbrustad@gmail.com,
+  SUPABASE_PASSWORD: Newhouse2025!,
+  supabase_access_token: sbp_5862ab9958a904e56c7ebf512f7aa68a0af73ba4
 };
