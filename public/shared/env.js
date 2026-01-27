d724bea4ff297d65a20321ee8c155794fd3ba02a
ericbrustad <ericbrustad@gmail.com>
2026-01-26 21:30:42 -0600

    Add SUPABASE_SERVICE_ROLE_KEY to env.js for admin operations

--- public/shared/env.js
+++ public/shared/env.js
@@ -3,6 +3,7 @@
 window.ENV = window.ENV || {};
 window.ENV.SUPABASE_URL = "https://siumiadylwcrkaqsfwkj.supabase.co";
 window.ENV.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpdW1pYWR5bHdjcmthcXNmd2tqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2NjMzMTMsImV4cCI6MjA4MTIzOTMxM30.sSZBsXyOOmIp2eve_SpiUGeIwx3BMoxvY4c7bvE2kKw";
+window.ENV.SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpdW1pYWR5bHdjcmthcXNmd2tqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NTY2MzMxMywiZXhwIjoyMDgxMjM5MzEzfQ.AwUvDEQNb_U04OveQ6Ia9wFgoIatwV6wigdwSQnsOP4";
 window.ENV.GOOGLE_MAPS_API_KEY = "AIzaSyAZhGX8NWGZd4p9OADvlqOgM6eZDwhQQh8";
 window.ENV.MAPBOX_TOKEN = "pk.eyJ1IjoiZXJpeGNvYWNoIiwiYSI6ImNtaDdocXI0NDB1dW4yaW9tZWFka3NocHAifQ.h1czc1VBwbBJQbdJTU5HHA";
 window.ENV.FORCE_DATABASE_ON_LOCALHOST = false;
